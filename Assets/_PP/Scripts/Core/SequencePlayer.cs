18c4221a6b6dd8e981ccdbba0dc09f6d34a24787
jeffpwang <jfywng@gmail.com>
2023-04-26 17:26:13 +0100

    fixes to sequence

--- Assets/_PP/Scripts/Core/SequencePlayer.cs
+++ Assets/_PP/Scripts/Core/SequencePlayer.cs
@@ -23,12 +23,6 @@ namespace Meta.PP
 
         public void PlayScene()
         {
-            // activate all sequences
-            foreach (CustomSequence sequence in sequences)
-            {
-                sequence.HideChildren();
-            }
-            
             // play scene automatically
             if (autoPlay)
             {
