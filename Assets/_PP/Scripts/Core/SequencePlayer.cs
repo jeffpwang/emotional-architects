583cce8885c847619d3d1f2da5fe6fd8b331a26e
Jeffrey Wang <jfywng@gmail.com>
2023-04-26 20:37:46 +0100

    pushes

--- Assets/_PP/Scripts/Core/SequencePlayer.cs
+++ Assets/_PP/Scripts/Core/SequencePlayer.cs
@@ -7,49 +7,9 @@ namespace Meta.PP
 {
     public class SequencePlayer : MonoBehaviour
     {
-        public string sceneId;
-        public bool autoPlay = false;
-
-        public float delayBeforePlay = 3f;
-        
         [Header("Sequences")] 
         public List<CustomSequence> sequences;
-        
-        private void OnDisable()
-        {
-            // AppManager.OnPlay -= PlayScene;
-            AppManager.OnStop -= EndScene;
-        }
 
-        public void PlayScene()
-        {
-            // play scene automatically
-            // if (autoPlay)
-            // {
-            //     StartCoroutine(PlayAfterDelay());
-            // }
-            
-            // begin listening to stop controls
-            // AppManager.OnPlay -= PlayScene;
-            AppManager.OnStop += EndScene;
-        }
-        
-        public void EndScene()
-        {
-            StopAllCoroutines();
-
-            AppManager.OnStop -= EndScene;
-        }
-
-        // IEnumerator PlayAfterDelay()
-        // {
-        //     yield return new WaitForSeconds(delayBeforePlay);
-        //     
-        //     AppManager.Instance.MoveToNextSequence(true);
-        //     
-        //     Debug.Log($"[CustomScene] Playing scene {AppManager.Instance.currentSceneSequence.name}");
-        // }
-        
         public void GoToNextSequence()
         {
             AppManager.Instance.MoveToNextSequence(true);
