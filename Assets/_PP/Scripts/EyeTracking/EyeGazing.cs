9e1cb67e0e8f40dfcaa5e969fe27c352614aee5f
matijamandurov <30595735+matijamandurov@users.noreply.github.com>
2023-04-26 13:35:27 +0200

    Finalized EyeGazing script

--- Assets/_PP/Scripts/EyeTracking/EyeGazing.cs
+++ Assets/_PP/Scripts/EyeTracking/EyeGazing.cs
@@ -5,13 +5,22 @@ namespace Meta.PP
     public class EyeGazing : MonoBehaviour
     {
         [SerializeField] private Transform _eyeTransform;
+        [SerializeField] private float _speed = 0.5f;
+
+        private RaycastHit _currentRaycastHit;
+        private RaycastHit _previousRaycastHit;
 
         private void Update()
         {
             Vector3 fwd = _eyeTransform.transform.TransformDirection(Vector3.forward);
-            if (Physics.Raycast(_eyeTransform.transform.position, fwd, out var objectHit, 50))
+            if (Physics.Raycast(_eyeTransform.transform.position, fwd, out _currentRaycastHit, 50))
             {
-                Debug.LogError(objectHit.transform.name);
+                Debug.LogError(_currentRaycastHit.transform.name);
+                if ((_currentRaycastHit.point - _previousRaycastHit.point).magnitude > _speed)
+                {
+                    Debug.LogError("Too fast");
+                }
+                _previousRaycastHit = _currentRaycastHit;
             }
         }
     }
