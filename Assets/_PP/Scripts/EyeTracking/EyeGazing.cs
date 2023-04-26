c991888cce58510b67ec8c33a5f86506f0f939bd
matijamandurov <30595735+matijamandurov@users.noreply.github.com>
2023-04-26 11:50:10 +0200

    Added basic eye gazing script

---
+++ Assets/_PP/Scripts/EyeTracking/EyeGazing.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Meta.PP
+{
+    public class EyeGazing : MonoBehaviour
+    {
+        [SerializeField] private Transform _eyeTransform;
+
+        private void Update()
+        {
+            Vector3 fwd = _eyeTransform.transform.TransformDirection(Vector3.forward);
+            if (Physics.Raycast(_eyeTransform.transform.position, fwd, out var objectHit, 50))
+            {
+                Debug.LogError("Seeing The cube");
+            }
+        }
+    }
+}
